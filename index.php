64cae1576ed3402700d77d5aa4508a4d489c560c
Justin D Howard unknown student <jdh26@afsconnect1.njit.edu>
2016-09-29 13:35:38 -0400

    learning php basics

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
 Echo 'Hello World';
-
+  // this is how you print stuff in php
+  $myvar = 'Some Text I want to store in a variable';
+// this is how you store stuff in a variable
+  echo '$myvar';
+  echo '<br>';
+  echo "$myvar";
+  // this is an example of of the difference between single and double quotes
 ?>
