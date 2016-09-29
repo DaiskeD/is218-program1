f6e4fdc6c2c41b97ba62b2c2fc6861d2bb0412e2
Justin D Howard unknown student <jdh26@afsconnect1.njit.edu>
2016-09-29 13:50:50 -0400

    Arrays

--- index.php
+++ index.php
@@ -6,5 +6,20 @@ Echo 'Hello World';
   echo '$myvar';
   echo '<br>';
   echo "$myvar";
-  // this is an example of of the difference between single and double quotes
+ // this is an example of of the difference between single and double quotes
+
+
+// this is an example of an array
+$myarray = array();
+$myarray[] = 'some value 1';
+$myarray[] = 'some value 2';
+$myarray[] = 'some value 3';
+
+print_r($myarray);
+
+$myAssoc = array('value1' => $myarray, 'value2' => $myarray);
+print_r($myAssoc);
+
+
+
 ?>
